1b3e5a6666f6a5864a1e0c8b020c78b623dcdc51
Udaykumar-56 <59176607+Udaykumar-56@users.noreply.github.com>
2019-12-31 12:19:22 +0530

    Add files via upload

---
+++ assign.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+printf("sum= %d\n",a+b);
+printf("diff= %d\n",a-b);
+printf("product= %d\n",a*b);
+printf("div= %d\n",a/b);
+}
+
---
+++ gh.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter a number:");
+	scanf("%d%d",&a);
+	printf("%d",a == b);
+	printf("%d",a/10 - a%10);
+}
